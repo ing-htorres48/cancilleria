3a59942c656b9fe5facd63394322bda749166c0f
gente-tech <gente.tech.co@gmail.com>
2025-12-25 11:51:49 -0500

    add fix block noticias 01

---
+++ web/themes/custom/cancilleria_d11/js/slider.js
@@ -0,0 +1,16 @@
+(function (Drupal, once) {
+  Drupal.behaviors.sliderSlick = {
+    attach(context) {
+      once('slider-slick', '.slick-slider', context).forEach((slider) => {
+        jQuery(slider).slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          dots: true,
+          autoplay: true,
+          autoplaySpeed: 4000
+        });
+      });
+    }
+  };
+})(Drupal, once);
