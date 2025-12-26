08eb55ceefe6ac6b665a9d720e15465caa7009a0
gente-tech <gente.tech.co@gmail.com>
2025-12-25 21:38:24 -0500

    add style slider

--- web/themes/custom/cancilleria_d11/js/slider.js
+++ web/themes/custom/cancilleria_d11/js/slider.js
@@ -1,13 +1,15 @@
 (function (Drupal, once, $) {
   Drupal.behaviors.sliderInterno = {
     attach(context) {
-      once('slider-interno', '.slick-slider', context).forEach((el) => {
+      once('slider-home', '.slick-slider-home', context).forEach((el) => {
         $(el).slick({
           slidesToShow: 1,
           slidesToScroll: 1,
           arrows: true,
           dots: true,
-          adaptiveHeight: true
+          adaptiveHeight: true,
+          autoplay: true,
+          autoplaySpeed: 5000
         });
       });
     }
