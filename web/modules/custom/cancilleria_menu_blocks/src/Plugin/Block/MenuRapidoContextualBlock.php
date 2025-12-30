80df1fed06db0801d474b91b1d77b8d7ea1fed82
Portales Web <portalesweb@colombiadigital.net>
2025-12-30 15:23:15 -0500

    Menú Rápido ajuste

--- web/modules/custom/cancilleria_menu_blocks/src/Plugin/Block/MenuRapidoContextualBlock.php
+++ web/modules/custom/cancilleria_menu_blocks/src/Plugin/Block/MenuRapidoContextualBlock.php
@@ -15,85 +15,78 @@ use Drupal\Core\Cache\Cache;
  */
 class MenuRapidoContextualBlock extends BlockBase {
 
-	protected function getTopLevelParent(string $menu_name, array $active_trail) {
-		$menu_link_manager = \Drupal::service('plugin.manager.menu.link');
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $menu_name = 'main';
 
-		foreach ($active_trail as $plugin_id) {
-			if (empty($plugin_id)) {
-				continue;
-			}
+    $menu_tree = \Drupal::menuTree();
+    $menu_link_manager = \Drupal::service('plugin.manager.menu.link');
+    $route_match = \Drupal::routeMatch();
 
-			if (!$menu_link_manager->hasDefinition($plugin_id)) {
-				continue;
-			}
+    $route_name = $route_match->getRouteName();
+    $route_parameters = $route_match->getRawParameters()->all();
 
-			$link = $menu_link_manager->createInstance($plugin_id);
+    // Buscar ítems de menú que apunten a ESTA ruta
+    $links = $menu_link_manager->loadLinksByRoute(
+      $route_name,
+      $route_parameters,
+      $menu_name
+    );
 
-			if ($link->getParent() === '' || str_starts_with($link->getParent(), $menu_name . ':')) {
-				return $plugin_id;
-			}
-		}
+    if (empty($links)) {
+      // Si la ruta no está directamente en el menú, no mostramos nada
+      return [];
+    }
 
-		return NULL;
-	}
+    // Tomamos el primer match (normalmente único)
+    $menu_link = reset($links);
+    $plugin_id = $menu_link->getPluginId();
 
+    // Parámetros del árbol
+    $parameters = new MenuTreeParameters();
+    $parameters
+      ->onlyEnabledLinks()
+      ->setRoot($plugin_id)
+      ->excludeRoot();
 
-	public function build() {
-		$menu_name = 'main';
+    $tree = $menu_tree->load($menu_name, $parameters);
 
-		$menu_tree = \Drupal::menuTree();
-		$menu_link_manager = \Drupal::service('plugin.manager.menu.link');
-		$active_trail = \Drupal::service('menu.active_trail')->getActiveTrailIds($menu_name);
+    // Si este ítem no tiene hijos, subimos al padre
+    if (empty($tree) && $menu_link->getParent()) {
+      $parameters
+        ->setRoot($menu_link->getParent())
+        ->excludeRoot();
 
-		$top_level_parent = $this->getTopLevelParent($menu_name, $active_trail);
+      $tree = $menu_tree->load($menu_name, $parameters);
+      $plugin_id = $menu_link->getParent();
+    }
 
-		$parameters = new MenuTreeParameters();
-		$parameters->onlyEnabledLinks();
+    if (empty($tree)) {
+      return [];
+    }
 
-		if (
-			!empty($top_level_parent) &&
-			is_string($top_level_parent) &&
-			$menu_link_manager->hasDefinition($top_level_parent)
-		) {
-			$parameters
-			->setRoot($top_level_parent)
-			->excludeRoot();
-		}
-		else {
-			$parameters->setMaxDepth(3);
-		}
+    $tree = $menu_tree->transform($tree, [
+      ['callable' => 'menu.default_tree_manipulators:checkAccess'],
+      ['callable' => 'menu.default_tree_manipulators:generateIndexAndSort'],
+    ]);
 
-		$tree = $menu_tree->load($menu_name, $parameters);
+    $menu_title = $menu_link_manager
+      ->createInstance($plugin_id)
+      ->getTitle();
 
-		if (!$tree) {
-			return [];
-		}
-
-		$tree = $menu_tree->transform($tree, [
-			['callable' => 'menu.default_tree_manipulators:checkAccess'],
-			['callable' => 'menu.default_tree_manipulators:generateIndexAndSort'],
-		]);
-
-		$parent_title = '';
-		if (!empty($top_level_parent) && $menu_link_manager->hasDefinition($top_level_parent)) {
-			$parent_title = $menu_link_manager
-			->createInstance($top_level_parent)
-			->getTitle();
-		}
-
-
-		return [
-		'#theme' => 'menu_rapido_contextual',
-		'#menu_title' => $parent_title,
-		'#tree' => $tree,
-		'#cache' => [
-			'contexts' => Cache::mergeContexts(
-			['route'],
-			['user.permissions']
-			),
-		],
-		];
-	}
-
+    return [
+      '#theme' => 'menu_rapido_contextual',
+      '#menu_title' => $menu_title,
+      '#tree' => $tree,
+      '#cache' => [
+        'contexts' => Cache::mergeContexts(
+          ['route', 'url.path'],
+          ['user.permissions']
+        ),
+      ],
+    ];
+  }
 
 }
